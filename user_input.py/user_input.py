fc58fc4987f098e79f9c31cd29c04f1e3bdb427d
Pascal Muthama <142976588+MuthamaPK@users.noreply.github.com>
2024-02-26 19:54:39 +0300

    Add files via upload

---
+++ user_input.py/user_input.py
@@ -0,0 +1,8 @@
+#prompt the user to enter their name
+name = input("Please enter your name:")
+#prompt the user to enter their age
+age = input("Please enter your age:")
+#prompt the user to enter their location
+location = input("Please enter your location:")
+#print personalized messsage thatt holds the name, age and location
+print(f"Hello {name}, you are {age} years old and you live in {location}.")
